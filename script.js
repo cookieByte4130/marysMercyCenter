23539781d9d648a91259aa939d74f64b72bf89e8
Amalia Bryant <amaliacbryant@gmail.com>
2021-01-20 20:21:42 -0800

    updated navbar added links

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//add active class to the section on the screen
+
+//after showing our story adjust the btn accordingly
+
+//customize the connect section
+
+//fix the donation section... mb a drop down menu? mb they can make multiple donations? shopping cart design? need to know more about the back end part
+
+//pair down the verbage
