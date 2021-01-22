dbf49e6ff47ac10c099bf23f9b16a2f3d60ce5b7
Amalia Bryant <amaliacbryant@gmail.com>
2021-01-21 20:43:35 -0800

    added containers and volunteer section

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 "use strict";
 
+//donate now button
+
+//donate margin-top shows when section is hidden
+{
+  /* <a
+  name="PayPalButton-1 $50 MMC DONATION MMCDONATION$50.00"
+  target="paypal"
+  href="/~site/Scripts_ForSale/ForSale.dll?CMD=CMDPayPalBuyButton&amp;PRODUCT_ID=4334493&amp;SHIPPING=false&amp;NOTE=false&amp;HSID=1761509101&amp;USERID=3268953"
+  onclick="window.open('/~site/Scripts_ForSale/ForSale.dll?CMD=CMDPayPalBuyButton&amp;PRODUCT_ID=4334493&amp;SHIPPING=false&amp;NOTE=false&amp;HSID=1761509101&amp;USERID=3268953','paypal','width=600,height=400,scrollbars,location,resizable,status');return false"
+></a>; */
+}
+
 //add active class to the section on the screen
 
 //after showing our story adjust the btn accordingly
 
-//customize the connect section
+//adjust font size to screen size
+
+//add FB link
 
 //fix the donation section... mb a drop down menu? mb they can make multiple donations? shopping cart design? need to know more about the back end part
 
