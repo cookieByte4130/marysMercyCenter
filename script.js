4fe5ce4e93d68c664fc3b7d949880e065514e184
Amalia Bryant <amaliacbryant@gmail.com>
2021-01-22 21:12:37 -0800

    added active section highlight

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict";
 
+//add active class to the section on the screen
+const links = document.querySelectorAll(".nav-item");
+const sections = document.querySelectorAll("section");
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 70 < sections[index].offsetTop) {}
+
+  links.forEach((link) => link.classList.remove("active"));
+  links[index].classList.add("active");
+}
+
+changeLinkState();
+window.addEventListener("scroll", changeLinkState);
+
 //donate now button
 
 //donate margin-top shows when section is hidden
@@ -11,8 +27,6 @@
   onclick="window.open('/~site/Scripts_ForSale/ForSale.dll?CMD=CMDPayPalBuyButton&amp;PRODUCT_ID=4334493&amp;SHIPPING=false&amp;NOTE=false&amp;HSID=1761509101&amp;USERID=3268953','paypal','width=600,height=400,scrollbars,location,resizable,status');return false"
 ></a>; */
 }
-
-//add active class to the section on the screen
 
 //after showing our story adjust the btn accordingly
 
