97df00e70304f803d914ddc60ac55aa16800aca2
Amalia Bryant <amaliacbryant@gmail.com>
2021-02-02 20:24:47 -0800

    add fact cards

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 "use strict";
+//add FB link
+
+//after showing our story adjust the btn accordingly
 
 //add active class to the section on the screen
 const links = document.querySelectorAll(".nav-item");
@@ -7,7 +10,7 @@ const sections = document.querySelectorAll("section");
 function changeLinkState() {
   let index = sections.length;
 
-  while (--index && window.scrollY + 70 < sections[index].offsetTop) {}
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
 
   links.forEach((link) => link.classList.remove("active"));
   links[index].classList.add("active");
@@ -15,8 +18,6 @@ function changeLinkState() {
 
 changeLinkState();
 window.addEventListener("scroll", changeLinkState);
-
-//donate now button
 
 //donate margin-top shows when section is hidden
 {
@@ -27,13 +28,3 @@ window.addEventListener("scroll", changeLinkState);
   onclick="window.open('/~site/Scripts_ForSale/ForSale.dll?CMD=CMDPayPalBuyButton&amp;PRODUCT_ID=4334493&amp;SHIPPING=false&amp;NOTE=false&amp;HSID=1761509101&amp;USERID=3268953','paypal','width=600,height=400,scrollbars,location,resizable,status');return false"
 ></a>; */
 }
-
-//after showing our story adjust the btn accordingly
-
-//adjust font size to screen size
-
-//add FB link
-
-//fix the donation section... mb a drop down menu? mb they can make multiple donations? shopping cart design? need to know more about the back end part
-
-//pair down the verbage
